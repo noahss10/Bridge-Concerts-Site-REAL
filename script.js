0b0e9a93a898f2f431af4d5b1e0fa4c5fc0b2b54
noahss10 <noahss10@icloud.com>
2026-01-20 00:11:23 +0000

    Update images

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ document.addEventListener('DOMContentLoaded', function() {
         {
             text: 'The entertainment industry differs across countries — from security regulations to production norms. With years of hands–on international experience, our team provides seamless communication between the artist team and local production. We are your link to ensuring smooth collaboration and the highest production standards.',
             author: 'WHY US',
-            image: '背景物料/微信图片_20260118000628_175_402.jpg'
+            image: 'Niji Wide.jpg'
         },
         {
             text: 'Combining technical expertise, tour management, production coordination, and creative insight, our team — from both China and the United States — works closely together to serve with professionalism, empathy, and flexibility, bridging regional differences with efficiency and understanding.',
             author: 'OUR TEAM',
-            image: '背景物料/微信图片_20260117195852_142_402.jpg'
+            image: 'Niji Narrow.jpg'
         }
     ];
 
